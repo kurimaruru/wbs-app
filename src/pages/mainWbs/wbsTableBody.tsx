50683e31bc15c2cb55e5955a643a8b6a2a56c1aa
kikkawa <wakaki555@kikkawaatsutaka.com>
2022-07-04 22:42:05 +0900

    テスト

--- src/pages/mainWbs/wbsTableBody.tsx
+++ src/pages/mainWbs/wbsTableBody.tsx
@@ -9,7 +9,7 @@ import {
 import { KeyboardArrowUp, KeyboardArrowDown } from '@material-ui/icons';
 import { useState } from 'react';
 import { AccountCircle } from '@material-ui/icons';
-import { DateTime } from 'luxon';
+// import { DateTime } from 'luxon';
 import { round } from 'lodash';
 import { WbsCommentTable } from './wbsCommentTable';
 import { WbsEditDialog } from '../../components/dialog/wbsEditDialog';
@@ -59,7 +59,7 @@ export const WbsTableBody = ({
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   // 現在時刻
-  const [nowTime, setNowTime] = useState(DateTime.now());
+  // const [nowTime, setNowTime] = useState(DateTime.now());
 
   // --------------------------------------------
   // 編集ダイアログ
@@ -84,13 +84,13 @@ export const WbsTableBody = ({
 
   const calculateDelay = (planFinishDay: string, resultFinishDay: string) => {
     let delayDays = '';
-    const finishDay = DateTime.fromSQL(planFinishDay);
-    const delay = nowTime.diff(finishDay, 'days');
+    // const finishDay = DateTime.fromSQL(planFinishDay);
+    // const delay = nowTime.diff(finishDay, 'days');
     // 進捗が遅れている場合はその日数を返す
-    if (round(delay.days) !== 0 && !resultFinishDay) {
-      delayDays = round(delay.days).toString();
-      return <span className={classes.delay}>{`${delayDays}日`}</span>;
-    }
+    // if (round(delay.days) !== 0 && !resultFinishDay) {
+    //   delayDays = round(delay.days).toString();
+    //   return <span className={classes.delay}>{`${delayDays}日`}</span>;
+    // }
     // 進捗が完了している場合
     if (resultFinishDay) {
       return <span className={classes.finish}>完了</span>;
